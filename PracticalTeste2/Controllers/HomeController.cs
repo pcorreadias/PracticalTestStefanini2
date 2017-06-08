fea6484978405344205db1650709ee9b88a0f1c6
Pedro Henrique Dias <pedro@whiletech.com.br>
2017-06-08 00:43:50 -0300

    Add project files.

---
+++ PracticalTeste2/Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using PracticalTeste2.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using static PracticalTeste2.Models.CustomerModel;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private StefaniniDBEntities _db = new StefaniniDBEntities();
+        private CustomerSearchModel _searchModel;
+
+        public ActionResult Index()
+        {
+            if (Session["IdLoggedUser"] != null)
+            {
+                if (Session["RoleLoggedUser"].Equals("Administrator"))
+                    return RedirectToAction("CustomerListAdmin");
+                else
+                    return RedirectToAction("CustomerListSeller");
+            }
+            else
+                return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserSys user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (StefaniniDBEntities dc = new StefaniniDBEntities())
+                {
+                    var v = dc.UserSys.Where(a => a.Email.Equals(user.Email) && a.Password.Equals(user.Password)).FirstOrDefault();
+
+                    if (v != null)
+                    {
+                        Session["IdLoggedUser"] = v.Id.ToString();
+                        Session["NameLoggedUser"] = v.Login.ToString();
+                        Session["EmailLoggedUser"] = v.Email.ToString();
+                        Session["RoleLoggedUser"] = v.UserRole.Name.ToString().Trim();
+                        ViewBag.ErrorClass = "";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.Message = "The e-mail and/or password entered is invalid. Please try again";
+                        ViewBag.ErrorClass = "formValidateError";
+                    }
+                }
+            }
+
+            return View(user);
+        }
+
+        public ActionResult CustomerListAdmin(CustomerSearchModel searchModel)
+        {
+            if (Session["IdLoggedUser"] != null)
+            {
+                _searchModel = searchModel;
+
+                var business = new CustomerModel();
+                var customers = business.GetCustomers(_searchModel);
+
+                ViewBag.ListOfGender = _db.Gender.ToList();
+                ViewBag.ListOfCity = _db.City.ToList();
+                ViewBag.ListOfRegion = _db.Region.ToList();
+                ViewBag.ListOfClassification = _db.Classification.ToList();
+                ViewBag.ListOfSeller = _db.UserSys.ToList();
+
+                return View(customers);
+            }
+            else
+                return RedirectToAction("Login");
+        }
+
+        public ActionResult CustomerListSeller(CustomerSearchModel searchModel, string sortOrder)
+        {
+            if (Session["IdLoggedUser"] != null)
+            {
+                _searchModel = searchModel;
+
+                ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+                ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+                searchModel.Seller = int.Parse(Session["IdLoggedUser"].ToString());
+                var business = new CustomerModel();
+                var customers = business.GetCustomers(_searchModel);
+
+                ViewBag.ListOfGender = _db.Gender.ToList();
+                ViewBag.ListOfCity = _db.City.ToList();
+                ViewBag.ListOfRegion = _db.Region.ToList();
+                ViewBag.ListOfClassification = _db.Classification.ToList();
+
+                return View(customers);
+            }
+            else
+                return RedirectToAction("Login");
+        }
+    }
+}
---
+++ PracticalTeste2/Models/CustomerModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticalTeste2.Models
+{
+    public class CustomerModel
+    {
+        public class CustomerSearchModel
+        {
+            public int? Id { get; set; }
+            public string Name { get; set; }
+            public int? Gender { get; set; }
+            public int? City { get; set; }
+            public int? Region { get; set; }
+            public DateTime? LastPurchaseFrom { get; set; }
+            public DateTime? LastPurchaseTo { get; set; }
+            public int? Classification { get; set; }
+            public int? Seller { get; set; }
+        }
+
+        //public class Gender
+        //{
+        //    public int? Id { get; set; }
+        //    public string Name { get; set; }
+        //}
+
+        //public class Gender
+        //{
+        //    public int? Id { get; set; }
+        //    public string Name { get; set; }
+        //}
+
+        private StefaniniDBEntities Context;
+        public CustomerModel()
+        {
+            Context = new StefaniniDBEntities();
+        }
+
+        public IQueryable<Customer> GetCustomers(CustomerSearchModel searchModel)
+        {
+            var result = Context.Customer.AsQueryable();
+
+            if (searchModel != null)
+            {
+                if (searchModel.Id.HasValue)
+                    result = result.Where(x => x.Id == searchModel.Id);
+                if (!string.IsNullOrEmpty(searchModel.Name))
+                    result = result.Where(x => x.Name.Contains(searchModel.Name));
+                if (searchModel.Gender.HasValue)
+                    result = result.Where(x => x.GenderId == searchModel.Gender);
+                if (searchModel.City.HasValue)
+                    result = result.Where(x => x.CityId == searchModel.City);
+                if (searchModel.Region.HasValue)
+                    result = result.Where(x => x.RegionId == searchModel.Region);
+                if (searchModel.LastPurchaseFrom.HasValue && searchModel.LastPurchaseTo.HasValue)
+                    result = result.Where(x => x.LastPurchase >= searchModel.LastPurchaseFrom && x.LastPurchase <= searchModel.LastPurchaseTo);
+                if (searchModel.Classification.HasValue)
+                    result = result.Where(x => x.ClassificationId == searchModel.Classification);
+                if (searchModel.Seller.HasValue)
+                    result = result.Where(x => x.UserId == searchModel.Seller);
+            }
+            return result;
+        }
+    }
+}
